9beeca0237e4551b9a72e5ad3daa9ff3e2ddc0a1
A1yona <alyona_zaichenko@mail.ru>
2020-06-29 17:36:46 +0500

    Update test.groovy

--- jenkinsfile/test.groovy
+++ jenkinsfile/test.groovy
@@ -20,15 +20,6 @@ pipeline {
                 }
             }
         }
-        stage('Create docker image'){
-            steps{
-                script{
-                    //sh "docker build ${WORKSPACE}/auto -t webapp"
-                    //sh "docker run -d webapp"
-                    //sh "docker exec -it webapp "df -h > ~/proc""
-                }
-            }
-        }
         
     }
 
